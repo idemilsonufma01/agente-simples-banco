d003f0030d9c356875bac1c0b2ed2d0c1391786c
idemilsonufma01 <90735723+idemilsonufma01@users.noreply.github.com>
2021-12-09 22:02:07 -0300

    Update Bancario.py
    adicionei mais alguns laços de repetição

--- Bancario.py
+++ Bancario.py
@@ -5,28 +5,51 @@ import time
 class Bancario(Banco): # Agente
     
     def atender(self):
-        meta = True # meta false quando atendimentos == 10
+        meta = True # meta false quando atendimentos == 9
 
         while meta:
-            atendimentos = 0
-            
+            #atendimentos = 0
             # Bancario.filaFormada
-            if len(fila) != 0 :
-                for atendimento in fila: # atende
-                    tamanho_lista = len(fila)
-                    
+            if (len(fila)) != 0 :
+                x = 0
+                while x <= len(fila) and meta == True: # atende
+                    #tamanho_lista = len(fila)
+                    atendimento = fila[x]
                     print(f"Obrigado {atendimento} pela preferência")
                     fila.pop(0)
-                
-                    time.sleep(0.1)
-                    atendimentos += 1
-                    print(atendimentos)
-            
-            # else:
-            #     if atendimentos == 9:
-            #         meta = False
-            #     Bancario.formaFila(self)                    
-            #     print("Sejam bem vindos!")
+                    print("Fila Atual: \n",fila)
+                    
+                    time.sleep(1)
+                    #atendimentos += 1
+                    #print(atendimentos) 
+                    if len(fila) == 0:
+                        time.sleep(1)
+                        y = 1
+                        while (y<=10):
+                            Bancario.formaFila(self) 
+                            print("Cliente na Fila")
+                        #print("valor X: ", y)
+                            time.sleep(1)
+                            y+=1
+                            time.sleep(2)
+
+                        
+                        z=0
+                        while(len(fila) != 0):
+                            atendimento = fila[0]
+                            print(f"Obrigado {atendimento} pela preferência")
+                            print("Fila Atual:", fila)
+                            time.sleep(1)
+                            fila.pop(0) 
+                            z+=1
+                            
+                            
+
+                        meta = False
+                        print("Fila Vazia: \n",fila)
+                        print("Atendimento Finalizado!")
+                           
+                  
 
 
     def filaFormada(self):
@@ -54,4 +77,3 @@ class Bancario(Banco): # Agente
            
 
         
-
